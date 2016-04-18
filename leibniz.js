46a6a50637d7be8171d2ff67d5c3ed26071b4bbf
ssmiler <spencer.kitchen@wsu.edu>
2016-04-18 12:43:28 -0700

    trying to fix const 4

--- leibniz.js
+++ leibniz.js
@@ -225,13 +225,11 @@ var diffSubtractRule = {
 //
 var diffConstRule = {
     pattern: function(target, table) {
-        return smatch(['DERIV', 'C?', 'T?'], target, table) && table.C !== "number" ||
+        return smatch(['DERIV', 'C?', 'T?'], target, table) && table.C === "object" &&
+        	   table.C.every(function(elem,index) {
+        	   	return smatch(elem, table.C[index])
+        	   });
 
-        	   smatch(['DERIV', 'C?', 'T?'], target, table) &&
-        	   (table.C instanceof Array &&
-        	   table.C.every(function(elem, index) {
-        	   	return smatch(elem, target[index])
-        	   }));
 
         //return false;
     },
